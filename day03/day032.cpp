79faaeeb348bb5d3f0b4f15bc821f89c23471daf
Miguel Rebollo <mrebollo@upv.es>
2024-12-03 10:06:22 +0100

    day 3 part 2 with failure (too high)

---
+++ day03/day032.cpp
@@ -0,0 +1,62 @@
+/*
+advent of code day 3 (2)
+extract substrings follwing a pattern
+usage of regular expressions with activation/deactivation (do()-don't())
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <regex>
+using namespace std;
+
+int prod(string s){
+    int a, b;
+    sscanf(s.c_str(), "mul(%d,%d)", &a, &b);
+    return a * b;
+}
+
+int process(string line){
+    int sumprod = 0;
+    string s = line;
+    // detecting three patterns mul() do() don't()
+    regex r("mul\\([0-9]+,[0-9]+\\)|do\\(\\)|don't\\(\\)");
+    smatch m; 
+    bool active = true;
+    while (regex_search(s, m, r)){
+        //reactivate with do()
+        if(m[0] == "do()"){
+            active = true;
+            //cout << "activated" << endl;
+        }
+        //deactivate with don't()
+        else if(m[0] == "don't()"){
+            active = false;
+            //cout << "deactivated" << endl;
+        }
+        // is a mult pattern, process if active
+        else if(active){
+            sumprod += prod(m[0]);
+            //cout << m[0] << " = " << prod(m[0]) << " processed" << endl;
+        }
+        else{
+            //cout << m[0] << " skipped" << endl;
+        }
+        s = m.suffix().str();
+    }
+    return sumprod;
+}
+
+int main() {
+    ifstream file("input.txt");
+    string line;
+    int sum = 0, prod, id = 0;
+    while(getline(file, line)){
+        prod = process(line);
+        cout << ++id << ": " << prod << endl;
+        sum += prod;
+    }
+    cout << "total: " << sum << endl;
+
+    return 0;
+}
