c4a407ec1a1a81bcf3b00073e4fd3514556ab368
mrebollo <mrebollo@upv.es>
2024-12-08 19:17:03 +0100

    day 7 part 1 completed

--- day07/day071.cpp
+++ day07/day071.cpp
@@ -8,42 +8,38 @@ using namespace std;
 
 // create a binary tree-like path
 // with all posible combination of op: (+) left, (*) right
-bool eval(vector<int> v, int idx, int res, int total){
-	if(idx==v.size())
+bool eval(vector<int> v, int idx, long long res, long long total){
+	if(idx==v.size()){
 		return res == total;
-	idx++;
-	return eval(v, idx, res+v[idx], total ) || 
-		    eval(v, idx, res*v[idx], total );
+	}
+	return eval(v, idx+1, res+v[idx], total ) || 
+		   eval(v, idx+1, res*v[idx], total );
 }
 
-
-//evaluate the expression to check
-//if there is an operation that gives result
-bool is_equal_to(string val, int result){
-	vector<int> v;
-	stringstream iss(val);
-	string data;
-	// generate vector from string
-    while (iss >> data){
-        v.push_back(stoi(data));
-    }
-   //check all combinations of + * operators
-	return eval(v, 0, v[0], result);
-}
 
 int main(){
-    fstream inputf("test.txt");
-    string line, values;
-	int result, sum = 0;
-    while(getline(inputf, line)){
-        sscanf(line, "%d:%s", &result, values);
-        if(is_equal_to(values, result)){
+    fstream inputf("input.txt");
+    string line, data;
+	vector<int> values;
+	//values can exceed int limits
+	long long result, sum = 0;
+     while(getline(inputf, line)){
+		stringstream ss(line);
+		//get the result 
+		getline(ss, data, ':');
+		result = stoll(data);
+		//get the values
+		while(ss >> data)
+			values.push_back(stoi(data));
+		//check if the result is possible
+        if(eval(values, 0, 0, result)){
 				sum += result;
 				cout << line << endl;
-			}
+		}
+		values.clear();
     }
     inputf.close();
-	cout << result << endl;
+	cout << sum << endl;
     return 0;
 
 }
