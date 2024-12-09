11bbb02aded9f7d149dad53b86900fbd1357a78e
Miguel Rebollo <mrebollo@upv.es>
2024-12-09 12:29:44 +0100

    day 9 data and code  prototype

---
+++ day09/day091.cpp
@@ -0,0 +1,33 @@
+/*
+advent of code day 9 (1)
+defragment disk
+vector manipulation
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+
+
+int main(){
+    string diskmap;
+    //load the disk map from file
+    fstream inputf("test.txt");
+    getline(inputf, diskmap);
+    inputf.close();
+
+    //expand format
+
+    //degfragment disk
+
+    //get checksum
+
+
+
+    return 0;
+}
