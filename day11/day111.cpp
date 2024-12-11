a45bd539a007d4ce3ba9056aac9a47a554206823
mrebollo <mrebollo@upv.es>
2024-12-11 07:36:12 +0100

    day 11 part 1 work on test, fail on input

---
+++ day11/day111.cpp
@@ -0,0 +1,89 @@
+/*
+advent of code day 11 (1)
+expand an array of integers
+0 -> 1
+even digits -> split in two
+other -> multiply by 2024
+*/
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+
+using namespace std;
+
+// load the initial vector
+vector<long long> load(string filename){
+    fstream inputf(filename);
+    vector<long long> v;
+    string line;
+    getline(inputf, line);
+    stringstream ss(line);
+    string num;
+    while(getline(ss, num, ' '))
+        v.push_back(stoi(num));
+    inputf.close();
+    return v;
+}
+
+
+// print the vector
+void print(vector<long long> &v){
+    for(int i = 0; i < v.size(); i++)
+        cout << v[i] << " ";
+    cout << endl;
+}
+
+// number of digits in a number
+inline int num_digits(long long n){
+    return ceil(log10(n));
+}
+
+// check if a number is even
+inline bool is_even(int n){
+    return n % 2 == 0;
+}
+
+// blink and apply rules
+vector<long long> blink(vector<long long> &stones){
+    vector<long long> newstones;
+    for(int i = 0; i < stones.size(); i++){
+        if(stones[i] == 0)
+            newstones.push_back(1);
+        else {
+            int digits = num_digits(stones[i]);
+            int half = pow(10,digits/2);
+            if(digits > 0 && is_even(digits)){
+                newstones.push_back(stones[i] / half);
+                newstones.push_back(stones[i] % half);
+            }
+            else
+                newstones.push_back(stones[i]*2024);
+        }
+    }
+    //print(newstones);
+    return newstones;
+}
+
+int main(){
+    vector<long long> stones;
+    stones = load("test.txt");
+    print(stones);
+
+    //expand the array
+    for(int i = 0; i < 25; i++){
+        stones = blink(stones);
+        if (i < 10)
+            print(stones);
+        cout << "num stones: " << stones.size() << endl;
+
+    }
+    cout << "final stones: " << stones.size() << endl;
+
+    return 0;
+}
+
