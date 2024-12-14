ed98fad760bd961244e0554763b84c2a8401b719
Miguel Rebollo <mrebollo@upv.es>
2024-12-14 17:10:18 +0100

    day 14 completed

--- day14/day142.cpp
+++ day14/day142.cpp
@@ -1,14 +1,7 @@
 /*
-advent of code day 14 (1)
-count movents until in christmas tree shape
-ej
-...1...
-..1.1..
-.1...1.
-1.....1
-...1...
-fist and last in center
-next = (x+1, y +/- 1)
+advent of code day 14 (2)
+count movents until a christmas tree shape appears
+(eastern egg)
 */
 
 #include <iostream>
@@ -39,20 +32,13 @@ struct robot{
     bool operator ==(const robot &b){
         return x == b.x && y == b.y;
     }
-    bool operator <(const robot &b) const{
-        return x < b.x || (x == b.x && y < b.y);
-    }
     robot operator+=(int t){
         x = abs((x+(vx * t)+MAXX) % MAXX);
         y = abs((y+(vy * t)+MAXY) % MAXY);
         return *this;
     }
-    robot operator-=(int t){
-        x = abs((x-(vx * t)+MAXX) % MAXX);
-        y = abs((y-(vy * t)+MAXY) % MAXY);
-        return *this;
-    }
 };
+
 
 int quadrant(robot r){
     if(r.x < CX && r.y < CY) return 0;
@@ -76,16 +62,10 @@ int safety(vector<robot> &rob){
 }
 
 //show robots' positions
-void plot(vector<robot> &rob, robot mark){
+void plot(vector<robot> &rob){
     int grid[MAXX][MAXY] = {0};
-    for(robot r
-    
-    : rob)
+    for(robot r: rob)
         grid[r.x][r.y]++;
-    if(mark.x != -1){
-        mark += 1;
-        grid[mark.x][mark.y] = 'x';
-    }
     for(int i = 0; i < MAXY; i++){
         for(int j = 0; j < MAXX; j++)
             if (grid[j][i] == 0)
@@ -96,29 +76,18 @@ void plot(vector<robot> &rob, robot mark){
     }   
 }
 
-//move robots t times forward
-void fwd(vector<robot> &rob, int t){
+
+// move robots t times 
+void move(vector<robot> &rob, int t){
     for(robot &r : rob)
-        r += t;
-}
-
-//move robots t times backward
-void bkwd(vector<robot> &rob, int t){
-    for(robot &r : rob)
-        r -= t;
-}
-
-
-vector<robot> move(vector<robot> &rob, int t){
-    if(t > 0)
-        fwd(rob, t);
-    else
-        bkwd(rob, t);
-    vector<robot> copy = rob;
-    return copy;
+        // review why it is not working
+        //r += t;
+        for(int i = 0; i < t; i++)
+            r += 1;
 }   
 
 
+// load the input file
 void load(vector<robot> &rob, string filename){
     fstream input(filename);
     string line;
@@ -131,44 +100,22 @@ void load(vector<robot> &rob, string filename){
     input.close();
 }
 
-bool is_tree(vector<robot> &rob, int id, int row){
-    if(row == MAXY-1)
-        return true;
-    if(rob[id].x != row)
-        return false;
-    if(rob[id].y == CY-id) 
-        return is_tree(rob, ++id, row);
-    if(rob[id].y == CY+id)
-        return is_tree(rob, ++id, ++row);
-    return false;
-}
 
-
-int move_until_tree(vector<robot> &rob){
-    for(int t = 1; ; t++){
-        vector<robot> step = move(rob, 1);
-        sort(rob.begin(), rob.end());
-        if(rob[0].x > 0 || rob[0].y != CY || rob[rob.size()-1].x < MAXX || rob[rob.size()-1].y != CY)
-            continue;
-        if(is_tree(step, 1, 0))
-            return t;
-    }
-}
-
+//save the current positions in a pgm bitmap
 void save(vector<robot> &rob, int t){
     char filename[100];
-    sprintf(filename, "~/Desktop/map/d14_%04d.pgm",t);
+    sprintf(filename, "../../../map/d14_%04d.pgm",t);
     fstream output(filename, ios::app);
-    output << "P5" << endl;
-    output << MAXX << " " << MAXY << endl;
+    output << "P2" << endl;
+    output << MAXY << " " << MAXX << endl;
     output << "255" << endl;
-    int grid[MAXX][MAXY] = {0};
+    vector<vector<int> > grid(MAXX, vector<int>(MAXY, 0));
     for(robot r: rob)
         grid[r.x][r.y]++;
     for(int i = 0; i < MAXY; i++){
         for(int j = 0; j < MAXX; j++)
-            output << (grid[j][i] == 0 ? 255 : 0);
-        cout << endl;
+            output << (grid[j][i] == 0 ? 255 : 0) << " ";
+        output << endl;
     }  
     output.close();
 }
@@ -183,7 +130,7 @@ int main(){
 #else
     load(rob, "input.txt");
 #endif
-    save(rob, 0);
+    //generates pictures to examinate the files 
     for(int t = 1; t < 10000; t++){
         move(rob,1);
         save(rob, t);
