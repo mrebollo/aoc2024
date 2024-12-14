c5e3a6a27baca06128c649b79986d947a4bf12a6
Miguel Rebollo <mrebollo@upv.es>
2024-12-14 15:49:47 +0100

    day 14 part 2 save in files

--- day14/day142.cpp
+++ day14/day142.cpp
@@ -39,9 +39,17 @@ struct robot{
     bool operator ==(const robot &b){
         return x == b.x && y == b.y;
     }
+    bool operator <(const robot &b) const{
+        return x < b.x || (x == b.x && y < b.y);
+    }
     robot operator+=(int t){
         x = abs((x+(vx * t)+MAXX) % MAXX);
         y = abs((y+(vy * t)+MAXY) % MAXY);
+        return *this;
+    }
+    robot operator-=(int t){
+        x = abs((x-(vx * t)+MAXX) % MAXX);
+        y = abs((y-(vy * t)+MAXY) % MAXY);
         return *this;
     }
 };
@@ -70,7 +78,9 @@ int safety(vector<robot> &rob){
 //show robots' positions
 void plot(vector<robot> &rob, robot mark){
     int grid[MAXX][MAXY] = {0};
-    for(robot r: rob)
+    for(robot r
+    
+    : rob)
         grid[r.x][r.y]++;
     if(mark.x != -1){
         mark += 1;
@@ -79,23 +89,31 @@ void plot(vector<robot> &rob, robot mark){
     for(int i = 0; i < MAXY; i++){
         for(int j = 0; j < MAXX; j++)
             if (grid[j][i] == 0)
-                cout << ".";
-            else if (grid[j][i] == 'x')
-                cout << "O";
+                cout << " ";
             else
-                cout << to_string(grid[j][i]);
+                cout << "#";
         cout << endl;
     }   
 }
 
-vector<robot> move(vector<robot> &rob, int t){
+//move robots t times forward
+void fwd(vector<robot> &rob, int t){
     for(robot &r : rob)
-        // check why it is not working
-        //r += t;
-        for(int i = 0; i < t; i++){
-            r += 1;
-            //plot(rob, r);
-        }
+        r += t;
+}
+
+//move robots t times backward
+void bkwd(vector<robot> &rob, int t){
+    for(robot &r : rob)
+        r -= t;
+}
+
+
+vector<robot> move(vector<robot> &rob, int t){
+    if(t > 0)
+        fwd(rob, t);
+    else
+        bkwd(rob, t);
     vector<robot> copy = rob;
     return copy;
 }   
@@ -113,18 +131,46 @@ void load(vector<robot> &rob, string filename){
     input.close();
 }
 
-bool is_tree(vector<robot> &rob){
-    return true;
+bool is_tree(vector<robot> &rob, int id, int row){
+    if(row == MAXY-1)
+        return true;
+    if(rob[id].x != row)
+        return false;
+    if(rob[id].y == CY-id) 
+        return is_tree(rob, ++id, row);
+    if(rob[id].y == CY+id)
+        return is_tree(rob, ++id, ++row);
+    return false;
 }
+
+
 int move_until_tree(vector<robot> &rob){
     for(int t = 1; ; t++){
-        robot step = move(rob, 1);
-        sort(rob.begin(), rob.end(), [](robot a, robot b){
-            return a.x < b.x;
-        });
-        if(is_tree(step, 0))
+        vector<robot> step = move(rob, 1);
+        sort(rob.begin(), rob.end());
+        if(rob[0].x > 0 || rob[0].y != CY || rob[rob.size()-1].x < MAXX || rob[rob.size()-1].y != CY)
+            continue;
+        if(is_tree(step, 1, 0))
             return t;
     }
+}
+
+void save(vector<robot> &rob, int t){
+    char filename[100];
+    sprintf(filename, "~/Desktop/map/d14_%04d.pgm",t);
+    fstream output(filename, ios::app);
+    output << "P5" << endl;
+    output << MAXX << " " << MAXY << endl;
+    output << "255" << endl;
+    int grid[MAXX][MAXY] = {0};
+    for(robot r: rob)
+        grid[r.x][r.y]++;
+    for(int i = 0; i < MAXY; i++){
+        for(int j = 0; j < MAXX; j++)
+            output << (grid[j][i] == 0 ? 255 : 0);
+        cout << endl;
+    }  
+    output.close();
 }
 
 
@@ -135,12 +181,14 @@ int main(){
 #ifdef _TEST
     load(rob, "test.txt");
 #else
-    load(rob, "test.txt");
+    load(rob, "input.txt");
 #endif
-    plot(rob, nomark);
-    int moves = move_until_tree(rob);
-    plot(rob, nomark);
-    cout << "Moves: " << moves << endl;
+    save(rob, 0);
+    for(int t = 1; t < 10000; t++){
+        move(rob,1);
+        save(rob, t);
+        cout << "Time: " << t << endl;
+    }
     return 0;
 }
 
