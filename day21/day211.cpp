8fb0839a32d9278b3d6d183b03ac04f298c1fa5c
Miguel Rebollo <mrebollo@upv.es>
2024-12-22 04:51:08 +0100

    day 21 part 1 distances correct now. Test fails in las case (overestimate)

--- day21/day211.cpp
+++ day21/day211.cpp
@@ -25,6 +25,7 @@ using namespace std;
 #define DW 0b00000100 //4, 8, 12
 #define LF 0b00010000 //16, 32, 48
 #define RG 0b01000000 //64, 128, 192
+char dirch[] = {'^', 'v', '<', '>', ' ', ' ', ' ', ' ', ' ', 'A'};
 
 // distance between two numbers in the kb
 //    789
@@ -33,13 +34,13 @@ using namespace std;
 //    *0A
 const int distKB[11][11] = {
     {0, UP+LF, UP, UP+RG, 2*UP+LF, 2*UP, 2*UP+RG, 3*UP+LF, 3*UP, 3*UP+RG, RG}, //from 0
-    {DW+LF, 0, RG, 2*RG, UP, UP+RG, UP+2*RG, 2*UP, 2*UP+RG, 2*UP+2*RG, DW+2*LF}, //from 1
+    {DW+RG, 0, RG, 2*RG, UP, UP+RG, UP+2*RG, 2*UP, 2*UP+RG, 2*UP+2*RG, DW+2*RG}, //from 1
     {DW, LF, 0, RG, UP+LF, UP, UP+RG, 2*UP+LF, 2*UP, 2*UP+RG, DW+RG}, //from 2
-    {DW+RG, 2*LF, LF, 0, UP+2*LF, UP+LF, UP, 2*UP+2*LF, 2*UP+LF, 2*UP, DW}, //from 3
+    {DW+LF, 2*LF, LF, 0, UP+2*LF, UP+LF, UP, 2*UP+2*LF, 2*UP+LF, 2*UP, DW}, //from 3
     {2*DW+RG, DW, DW+RG, DW+2*RG, 0, RG, 2*RG, UP, UP+RG, UP+2*RG, 2*DW+2*RG}, //from 4
     {2*DW, DW+LF, DW, DW+RG, LF, 0, RG, UP+LF, UP, UP+RG, 2*DW+RG}, //from 5
-    {2*DW+LF, DW+2*LF, 2*DW+LF, 2*DW, 2*LF, LF, 0, UP+2*LF, UP+LF, UP, 2*DW}, //from 6
-    {3*DW+RG, 2*DW, 2*DW+RG, 2*DW+2*RG, DW, DW+RG, DW+2*RG, 0, RG, 2*RG, 3*DW+RG}, //from 7
+    {2*DW+LF, DW+2*LF, DW+LF, DW, 2*LF, LF, 0, UP+2*LF, UP+LF, UP, 2*DW}, //from 6
+    {3*DW+RG, 2*DW, 2*DW+RG, 2*DW+2*RG, DW, DW+RG, DW+2*RG, 0, RG, 2*RG, 3*DW+2*RG}, //from 7
     {3*DW, 2*DW+LF, 2*DW, 2*DW+RG, DW+LF, DW, DW+RG, LF, 0, RG, 3*DW+RG}, //from 8
     {3*DW+LF, 2*DW+2*LF, 2*DW+LF, 2*DW, DW+2*LF, DW+LF, DW, 2*LF, LF, 0, 3*DW}, //from 9
     {LF, UP+2*LF, UP+LF, UP, 2*UP+2*LF, 2*UP+LF, 2*UP, 3*UP+2*LF, 3*UP+LF, 3*UP, 0} //from A
@@ -50,12 +51,13 @@ const int distKB[11][11] = {
 const int distDIR[11][11] = {
     {0, DW, DW+LF, DW+RG, 0, 0, 0, 0, 0, 0, RG}, //from UP
     {UP, 0, LF, RG, 0, 0, 0, 0, 0, 0, UP+RG}, //from DW
-    {UP+RG, RG, 0, 2*LF, 0, 0, 0, 0, 0, 0, UP+2*LF}, //from LFT
+    {UP+RG, RG, 0, 2*RG, 0, 0, 0, 0, 0, 0, UP+2*RG}, //from LFT
     {UP+LF, LF, 2*LF, 0, 0, 0, 0, 0, 0, 0, UP},  //from RGT
     {0}, {0}, {0}, {0}, {0}, {0}, //empty
     {LF, DW+LF, DW+2*LF, DW, 0, 0, 0, 0, 0, 0, 0} //from A
 };
 
+string output;
 
 class Keypad{
     private:
@@ -86,12 +88,17 @@ Keypad::Keypad(const int kb[][11], Keypad *ctrl = nullptr){
 int Keypad::dist(int from, int to){
     int bitw = kb[from][to];
     int moves = 0;
-    for(int i = 0; i < 4; i++)
-        moves += (bitw >> i * 2) & 3;
+    for(int i = 0; i < 4; i++){
+        int m = (bitw >> i * 2) & 3;
+        moves += m;
+        for(int j = 0; j < m; j++)
+            output.push_back(dirch[i]);
+    }
     //moves += (bitw >> 0) & 3; //UPs 1
     //moves += (bitw >> 2) & 3; //DWs 4
     //moves += (bitw >> 4) & 3; //LFs 16
     //moves += (bitw >> 6) & 3; //RGs 64
+    output += 'A';
     return moves;
 }
 
@@ -135,6 +142,8 @@ int Keypad::directions(int from, int to, Keypad *controlled){
 
 int Keypad::type(string code){
     int total_moves = 0, to;
+    output.clear();
+    cout << code << endl;
     for(char c: code){
         // convert the character to a number  
         to = (c == 'A') ? A : c - '0';
@@ -143,6 +152,7 @@ int Keypad::type(string code){
         // update the current position
         at = to;
     }
+    cout << output << endl;
     return total_moves;
 }
 
@@ -160,7 +170,7 @@ int main() {
     Keypad radiation(distDIR, &cold);
     Keypad numeric(distKB, &radiation);
     // use numeric keypad directly
-    // Keypad numeric(distKB);
+    //Keypad numeric(distKB);
     
     fstream inputf("test.txt");
     int moves, complexity = 0;
@@ -173,3 +183,13 @@ int main() {
     cout << "total complexity: " << complexity << endl;
     return 0;
 }
+
+/* test output
+029A: <vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A 68
+980A: <v<A>>^AAAvA^A<vA<AA>>^AvAA<^A>A<v<A>A>^AAAvA<^A>A<vA>^A<A>A 60
+179A: <v<A>>^A<vA<A>>^AAvAA<^A>A<v<A>>^AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A 68
+456A: <v<A>>^AA<vA<A>>^AAvAA<^A>A<vA>^A<A>A<vA>^A<A>A<v<A>A>^AAvA<^A>A 64
+379A: <v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A 64
+
+
+*/
