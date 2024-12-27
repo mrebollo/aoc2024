f715d54467acdf2ca1969339004949482d303fc3
mrebollo <mrebollo@upv.es>
2024-12-27 10:49:18 +0100

    day 13 completed

--- day13/day132.cpp
+++ day13/day132.cpp
@@ -4,15 +4,16 @@ claws machine
 move the claw to the target position to win the prize
 two butons A, B with different speeds and costs (A 3tk, B 1tk)
 but sum 10000000000000 to the prize's coordinates
--> solve analitycally
+-> solve analitycally 
+
 */
 
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
-//#include <linalg>
-//#include <algorithm>
+#include "linalg.h"
+
 
 #define EXCESS 10000000000000
 #define MAX_VALUE 99999
@@ -23,7 +24,7 @@ but sum 10000000000000 to the prize's coordinates
 using namespace std;
 
 struct coor{
-    long x, y;
+    long long x, y;
     string show(){
         return "(" + to_string(x) + "," + to_string(y) + ")";
     }
@@ -46,11 +47,9 @@ coor origin = {0, 0};
 class Claws{
 private:
     coor A, B, prize;
-    int moves;
-    long cost;
+    long long  cost;
 public:
     Claws(coor A, coor B, coor prize);
-    int move_claw(vector<vector<int> > &M, coor pos, int nA, int nB);
     void show();
 };
 
@@ -59,31 +58,6 @@ Claws::Claws(coor A, coor B, coor prize){
     this->A = A;
     this->B = B;
     this->prize = prize;
-    moves = 0;
-    cost = 0;
-}
-
-
-// move the claw to the prize position
-int Claws::move_claw(vector<vector<int> > &M, coor pos, int nA, int nB){
-    if(pos < origin || nA > MAX_MOVES || nB > MAX_MOVES){
-        return MAX_VALUE; //infinite
-    }
-    if(M[pos.x][pos.y] != -1){
-        return M[pos.x][pos.y];
-    }   
-    int cost = 0;
-    if(pos == origin){
-        M[0][0] = 0;
-        //return 0;
-    }
-    else{
-        long pressA = move_claw(M, pos - A, nA+1, nB) + TKN_A;
-        long pressB = move_claw(M, pos - B, nA, nB+1) + TKN_B;
-        M[pos.x][pos.y] = min(pressA, pressB);
-        //return min(pressA, pressB);
-    }
-    return M[pos.x][pos.y];
 }
 
 
@@ -96,34 +70,51 @@ void Claws::show(){
 
 
 // get the prize position at minimum cost
-int get_prize(coor A, coor B, coor prize){
+long long get_prize(coor A, coor B, coor prize){
     //define equation system for simplex problem
+    // x: A moves, y: B moves
     // min cost = 3x + y
     // sujeto a las restricciones
     // x * A.x + y * B.x = prize.x
     // x * A.y + y * B.y = prize.y
     // x, y >= 0
-
-
-
-    return cost;
+    // use the graphical method: intersection of the lines
+    // x = (prize.x - y * B.x) / A.x = (prize.y - y * B.y) / A.y 
+    // A.y(prize.x - y * B.x) = A.x(prize.y - y * B.y)
+    // A.y * prize.x - A.y * y * B.x = A.x * prize.y - A.x * y * B.y
+    // A.y * y * B.x - A.x * y * B.y = A.y * prize.x - A.x * prize.y
+    lldiv_t ydiv = lldiv((A.y * prize.x - A.x * prize.y), (A.y * B.x - A.x * B.y));
+    // substitute in the first equation
+    lldiv_t x1div = lldiv((prize.x - ydiv.quot * B.x), A.x);
+    lldiv_t x2div = lldiv((prize.y - ydiv.quot * B.y), A.y);
+    //cout << "A: " << A.show() << " B: " << B.show() << " Prize: " << prize.show() << " Moves: " << x << " " << y << endl;
+    cout << "A: " << A.show() << " B: " << B.show() << " Prize: " << prize.show() << " Moves: " << x1div.quot << "|" << x2div.quot << " "<< ydiv.quot << endl;
+    if( ydiv.rem >0 || x1div.rem > 0 || 
+        x1div.quot < 0 || ydiv.quot < 0 ){
+        //|| x1div.quot > MAX_MOVES || ydiv.quot > MAX_MOVES){
+        cout << "\t-> No solution" << endl;
+        return 0;
+    }
+    else 
+        cout << "\t-> Cost: " << x1div.quot * TKN_A + ydiv.quot * TKN_B << endl;
+        return x1div.quot * TKN_A + ydiv.quot * TKN_B;
 }
 
 
 int main() {
     //load the input
-    fstream input("test.txt");
+    fstream input("input.txt");
     string line;
     char btn[20], name; 
     coor A, B, prize;
-    int tokens = 0;
+    long long tokens = 0;
     while(getline(input, line)){
-        sscanf(line.c_str(), "%s %c: X+%ld, Y+%ld", btn, &name, &A.x, &A.y);
+        sscanf(line.c_str(), "%s %c: X+%lld, Y+%lld", btn, &name, &A.x, &A.y);
         getline(input, line);
-        sscanf(line.c_str(), "%s %c: X+%ld, Y+%ld", btn, &name, &B.x, &B.y);
+        sscanf(line.c_str(), "%s %c: X+%lld, Y+%lld", btn, &name, &B.x, &B.y);
         getline(input, line);
         //be careful: this include ":" in btn since read until space
-        sscanf(line.c_str(), "%s X=%ld, Y=%ld", btn, &prize.x, &prize.y);
+        sscanf(line.c_str(), "%s X=%lld, Y=%lld", btn, &prize.x, &prize.y);
         getline(input, line);
         prize.x += EXCESS;
         prize.y += EXCESS;
